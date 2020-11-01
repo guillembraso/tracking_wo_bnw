cdba41835e8ff546a7ef9c32308ff4bc29b713c5
Guillem Braso Andilla <brasoand@atcremers82.vision.in.tum.de>
2020-11-01 18:07:39 +0100

    Added tracktor support for MOT20, got rid of unnecessary classes

--- src/tracktor/datasets/mot_sequence.py
+++ src/tracktor/datasets/mot_sequence.py
@@ -234,8 +234,7 @@ class MOT17Sequence(Dataset):
                     y2 = bb[3]
                     writer.writerow([frame+1, i+1, x1+1, y1+1, x2-x1+1, y2-y1+1, -1, -1, -1, -1])
 
-
-class MOT19Sequence(MOT17Sequence):
+class MOT20Sequence(MOT17Sequence):
 
     def __init__(self, seq_name=None, dets='', vis_threshold=0.0,
                  normalize_mean=[0.485, 0.456, 0.406],
@@ -249,8 +248,8 @@ class MOT19Sequence(MOT17Sequence):
         self._dets = dets
         self._vis_threshold = vis_threshold
 
-        self._mot_dir = osp.join(DATA_PATH, 'MOT19')
-        self._mot17_label_dir = osp.join(DATA_PATH, 'MOT19')
+        self._mot_dir = osp.join(DATA_PATH, 'MOT20')
+        self._mot17_label_dir = osp.join(DATA_PATH, 'MOT20')
 
         # TODO: refactor code of both classes to consider 16,17 and 19
         self._label_dir = osp.join(DATA_PATH, 'MOT16Labels')
@@ -270,68 +269,21 @@ class MOT19Sequence(MOT17Sequence):
             self.data = []
             self.no_gt = True
 
+
+        ########################
+
+        seq_name = self._seq_name
+        train_test_str =  'train' if seq_name in self._train_folders else 'test'
+        self.seq_path = osp.join(self._mot_dir, train_test_str, seq_name)
+
+    def __str__(self):
+        return self._seq_name
+
     def get_det_file(self, label_path, raw_label_path, mot17_label_path):
         # FRCNN detections
-        if "MOT19" in self._seq_name:
+        if "MOT20" in self._seq_name:
             det_file = osp.join(mot17_label_path, self._seq_name, 'det', 'det.txt')
         else:
             det_file = ""
         return det_file
 
-    def write_results(self, all_tracks, output_dir):
-        assert self._seq_name is not None, "[!] No seq_name, probably using combined database"
-
-        #if not os.path.exists(output_dir):
-        #    os.makedirs(output_dir)
-
-        #file = osp.join(output_dir, f'{self._seq_name}.txt')
-
-        print("[*] Writing to: {}".format(output_dir))
-
-        with open(output_dir, "w") as of:
-            writer = csv.writer(of, delimiter=',')
-            for i, track in all_tracks.items():
-                for frame, bb in track.items():
-                    x1 = bb[0]
-                    y1 = bb[1]
-                    x2 = bb[2]
-                    y2 = bb[3]
-                    writer.writerow(
-                        [frame + 1, i + 1, x1 + 1, y1 + 1, x2 - x1 + 1, y2 - y1 + 1, -1, -1, -1, -1])
-
-
-class MOT17LOWFPSSequence(MOT17Sequence):
-
-    def __init__(self, split, seq_name=None, dets='', vis_threshold=0.0,
-                 normalize_mean=[0.485, 0.456, 0.406],
-                 normalize_std=[0.229, 0.224, 0.225]):
-        """
-        Args:
-            seq_name (string): Sequence to take
-            vis_threshold (float): Threshold of visibility of persons above which they are selected
-        """
-        self._seq_name = seq_name
-        self._dets = dets
-        self._vis_threshold = vis_threshold
-
-        self._mot_dir = osp.join(DATA_PATH, 'MOT17_LOW_FPS', f'MOT17_{split}_FPS')
-        self._mot17_label_dir = osp.join(DATA_PATH, 'MOT17_LOW_FPS', f'MOT17_{split}_FPS')
-
-        # TODO: refactor code of both classes to consider 16,17 and 19
-        self._label_dir = osp.join(DATA_PATH, 'MOT16Labels')
-        self._raw_label_dir = osp.join(DATA_PATH, 'MOT16-det-dpm-raw')
-
-        self._train_folders = os.listdir(os.path.join(self._mot_dir, 'train'))
-        self._test_folders = os.listdir(os.path.join(self._mot_dir, 'test'))
-
-        self.transforms = Compose([ToTensor(), Normalize(normalize_mean,
-                                                         normalize_std)])
-
-        if seq_name is not None:
-            assert seq_name in self._train_folders or seq_name in self._test_folders, \
-                'Image set does not exist: {}'.format(seq_name)
-
-            self.data, self.no_gt = self._sequence()
-        else:
-            self.data = []
-            self.no_gt = True
