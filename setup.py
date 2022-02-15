e6c67c54c115d53f609a55cb730e81967c582075
Guillem Brasó <32915870+guillembraso@users.noreply.github.com>
2022-02-15 12:01:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tracktor',
-      packages=['tracktor', 'tracktor.datasets],
+      packages=['tracktor', 'tracktor.datasets'],
       package_dir={'':'src'},
       version='0.0.1',
       install_requires=[],)
